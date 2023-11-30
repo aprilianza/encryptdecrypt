761a5ed480c11ab8d7da75bca07cce7af6fe4811
Aprilianza <aprilianzamuhammadyusup@gmail.com>
2023-11-30 17:33:37 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,42 @@
+from cryptography.fernet import Fernet
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+key = load_key()
+fer = Fernet(key)
+
+
+def view():
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User:", user, "| Password:",
+                  fer.decrypt(passw.encode()).decode())
+
+
+def add():
+    name = input('Account Name: ')
+    pwd = input('Password: ')
+
+    with open('passwords.txt', 'a') as f:
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
+
+
+while True :
+    mode = input('apakah kamu ingin menambah password baru atau melihat password sebelumnya(lihat, tambah), q untuk keluar : ').lower()
+    if mode == "q":
+        break
+    
+    if mode == "tambah":
+        add() 
+    elif mode == "lihat":
+        view()
+    else:
+        print('tidak valid, coba lagi!')
+        continue
